f412f566c4b46afb856dfc66f70683018b043d94
agungdhewe <agung_dhewe@yahoo.com>
2025-09-30 17:05:26 +0700

    dw 20250930 1705

--- src/generator/createModuleDetilEditHtml.js
+++ src/generator/createModuleDetilEditHtml.js
@@ -1,4 +1,4 @@
-import { kebabToCamel, isFileExist, getSectionData } from './helper.js'
+import { kebabToCamel, isFileExist, getSectionData, createAdditionalAttributes } from './helper.js'
 import { fileURLToPath } from 'url';
 import path from 'path'
 import fs from 'fs/promises'
@@ -32,9 +32,61 @@ export async function createModuleDetilEditHtml(context, options) {
 			}
 			context.postMessage({message: `generating file: '${targetFile}`})  // reporting progress to parent process
 
+			// start geneate program code
+			const entityData = context.entities[entityName]
+			const sectionData = getSectionData(moduleName, entityName, entityData, 'edit')
+			const primaryKeyFieldData = entityData.Items[sectionData.primaryKey] 
+			const primaryKeyName = primaryKeyFieldData.input_name
+			const primaryKeyElementId = `${modulePart}-${primaryKeyName}`
+
+			const autoid = true // untuk detil, pasti autoid
+
+			
+			// ambil data field header
+			const fields = []
+			for (var fieldName in entityData.Items) {
+				const item = entityData.Items[fieldName]
+
+				// if (item.data_fieldname=='grouptype_id') {
+				// 	console.log(item)
+				// }
+
+				if (!item.showInForm) {
+					continue
+				}
+
+				const component = item.component
+				const fieldname = item.data_fieldname
+				const elementId = `${modulePart}-${item.input_name}`
+				const placeholder = item.input_placeholder
+				const label = item.input_label
+				const binding = item.data_fieldname
+				const additionalAttributes = createAdditionalAttributes(item)
+				const cssContainer = item.input_containercss.trim() == '' ? 'input-field' : `input-field ${item.input_containercss.trim()}` 
+
+				fields.push({  
+					component,
+					cssContainer,
+					fieldname,
+					elementId,
+					placeholder,
+					label,
+					binding,
+					additionalAttributes
+				})
+
+			}
 
 
 			const variables = {
+				title: title,
+				moduleName: moduleName,
+				modulePart: modulePart,
+				moduleSection:  kebabToCamel(`${moduleName}-${sectionName}`),
+				section: sectionData,
+				autoid,
+				primaryKeyElementId: primaryKeyElementId,
+				fields: fields,
 			}
 
 			const tplFilePath = path.join(__dirname, 'templates', 'moduleDetilEdit.html.ejs')
--- src/generator/createModuleDetilEditMjs.js
+++ src/generator/createModuleDetilEditMjs.js
@@ -14,7 +14,15 @@ export async function createModuleDetilEditMjs(context, options) {
 	const sectionPart = 'edit'
 
 	try {
+
+		const headerEntityData = context.entities['header']
+		const headerModulePartEdit = kebabToCamel(`${moduleName}-header-edit`)
+		const headerPrimaryKey = headerEntityData.pk
+
+
 		for (let entityName in context.entities) {
+			const entityData = context.entities[entityName]
+
 			// process selain header
 			if (entityName=='header') {
 				continue
@@ -35,10 +43,95 @@ export async function createModuleDetilEditMjs(context, options) {
 			context.postMessage({message: `generating file: '${targetFile}`})
 
 
+			const tablename = entityData.table
+
+			const fields = []
+			const fieldHandles = []
+			const defaultInits = []
+			for (var fieldName in entityData.Items) {
+				const item = entityData.Items[fieldName]
+
+				if (!item.showInForm) {
+					continue
+				}
+
+
+				const component = item.component
+				const fieldname = item.data_fieldname
+				const inputname = item.input_name
+				const elementId = `${modulePart}-${item.input_name}`
+
+
+				// setup handles
+				const handles = []
+				for (let eventname in item.Handle) {
+					let createhandle = item.Handle[eventname]
+					if (createhandle) {
+						if (eventname=='selecting' && component=='Combobox') {
+							handles.push({
+								eventname,
+								appId: item.Reference.loaderApiModule,
+								path: item.Reference.loaderApiPath,
+								field_value: item.Reference.bindingValue,
+								field_text: item.Reference.bindingText, 
+							})
+						} else {
+							handles.push({eventname})
+						}
+					}
+				}
+
+				if (handles.length>0) {
+					fieldHandles.push({component, inputname, handles})
+				}
+
+
+				// setup default values
+				let setdefault
+				if (item.data_defaultvalue != '') {
+					if (item.component=='Datepicker' || item.component=='Timepicker') {
+						setdefault = `${item.name }: new Date()`
+					} else if (item.component=='Numberbox') {
+						setdefault = `${item.name }: ${item.data_defaultvalue}`
+					} else if (item.component=='Checkbox') {
+						if (item.data_defaultvalue==='true' || item.data_defaultvalue==='checked' || item.data_defaultvalue==='1') {
+							setdefault = `${item.name }: true`
+						}
+					} else {
+						setdefault = `${item.name }: '${item.data_defaultvalue}'`
+					}
+				}
+
+				if (setdefault!=null) {
+					defaultInits.push(setdefault)
+				}
+
+
+
+
+
+				// add to field config data	
+				fields.push({  
+					component,
+					fieldname,
+					inputname,
+					elementId
+				})
+			}			
 
 
 			const variables = {
-
+				title,
+				tablename,
+				modulePart,
+				moduleName,
+				sectionName,
+				headerPrimaryKey,
+				moduleSection:  kebabToCamel(`${moduleName}-${sectionName}`),
+				moduleList: kebabToCamel(`${moduleName}-${sectionName}-list`),
+				fields,
+				fieldHandles,
+				defaultInits,
 			}
 
 			const tplFilePath = path.join(__dirname, 'templates', 'moduleDetilEdit.mjs.ejs')
